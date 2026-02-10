e7fa2e821e5f93dadfdf65acb538f360239e4b83
AbuUsama <AbuUsama@DAOresear.ch>
2026-02-10 15:26:12 +0000

    Revert "feat(design-system): add base primitives and stories"
    This reverts commit b428f0b98e1cadef7f3504c7a58644c54e0817c7.

--- apps/web/src/design-system/primitives/index.ts
+++
@@ -1,9 +0,0 @@
-export { Badge, type BadgeProps, type BadgeTone } from "./Badge";
-export {
-	Button,
-	type ButtonProps,
-	type ButtonSize,
-	type ButtonVariant,
-} from "./Button";
-export { Card, type CardElement, type CardProps, type CardTone } from "./Card";
-export { Input, type InputProps } from "./Input";
