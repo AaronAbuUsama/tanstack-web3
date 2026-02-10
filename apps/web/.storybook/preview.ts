b6afd22d8f2e226f47a462c77920d7537183e26c
AbuUsama <AbuUsama@DAOresear.ch>
2026-02-10 15:26:12 +0000

    Revert "feat(design-system): add command-center foundation tokens"
    This reverts commit fc0ffa75fc2aa49c0c5aaa90ad8837df45623dd6.

--- apps/web/.storybook/preview.ts
+++ apps/web/.storybook/preview.ts
@@ -1,12 +1,11 @@
 import type { Preview } from '@storybook/react-vite'
-import '../src/styles.css'
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
+       color: /(background|color)$/i,
+       date: /Date$/i,
       },
     },
 
@@ -14,17 +13,9 @@ const preview: Preview = {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
       // 'off' - skip a11y checks entirely
-      test: 'todo',
-    },
-    layout: 'fullscreen',
-    backgrounds: {
-      default: 'canvas',
-      values: [
-        { name: 'canvas', value: 'var(--ds-color-bg-canvas)' },
-        { name: 'surface', value: 'var(--ds-color-bg-surface)' },
-      ],
-    },
+      test: 'todo'
+    }
   },
-}
+};
 
-export default preview
+export default preview;
